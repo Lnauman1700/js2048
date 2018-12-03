1edfa5c65f13cf6b4d15f783a10b1f29a7071e69
Lucas Nauman <Lnauman1700@gmail.com>
2018-12-03 14:12:47 -0500

    initialized the main.test.js, package.json, and the .gitignore files

---
+++ main.test.js
@@ -0,0 +1,15 @@
+const {
+  createGrid,
+  updateGrid,
+  moveUp,
+  moveRight,
+  moveLeft,
+  moveDown
+} = require("./main");
+
+
+describe(moveUp, () => {
+  test("move 1 slot upwards", () => {
+    
+  });
+});
